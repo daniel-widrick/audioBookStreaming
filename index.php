6142d24d4d9debbdb2ac528984dbc33b5e69b1e4
Daniel Widrick <daniel@widrick.net>
2018-04-04 02:30:44 +0000

    working login. todo: db skeleton

--- index.php
+++ index.php
@@ -3,10 +3,33 @@
 session_start();
 
 include 'themes/dg/main.html';
-if(!isset($_SESSION['username']) && !isset($_POST['submit']) )
+if(!isset($_SESSION['username']) && (!isset($_POST['username']) || !isset($_POST['password'])) )
 {
 	include 'themes/dg/login.html';
 	exit;
-}
+}else if( isset($_POST['username']) && isset($_POST['password']) ) 
+{
+	//Check login credentials
+	$pdo = new PDO('sqlite:database/database.sqlite3');
+	$saltQuery = "SELECT salt from users where username = :username";
+	$saltStmt = $pdo->prepare($saltQuery);
+	$saltStmt->bindParam(':username',$_POST['username']);
+	$saltStmt->execute();
+	$salt = $saltStmt->fetch()["salt"];
+
+	$passhash = md5($_POST['password'] . $salt);
+	$usernameQuery = "SELECT username from users where username = :username and password = :password";
+	$usernameStmt = $pdo->prepare($usernameQuery);
+	$usernameStmt->bindParam(':username',$_POST['username']);
+	$usernameStmt->bindParam(':password',$passhash);
+	$usernameStmt->execute();
+	$username = $usernameStmt->fetch()["username"];
+
+	if($username === null)
+	{
+		die("Authentication Failure. TODO: redirect");
+	}
+	$_SESSION['username'] = $username;
+}	
 
 
