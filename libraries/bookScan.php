b6a6e2707b75d7ad705d32d459b04cf2f9f4e793
Daniel Widrick <daniel@widrick.net>
2018-04-04 18:16:40 +0000

    prototype to pull info from google books api

--- libraries/bookScan.php
+++ libraries/bookScan.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DIRECTORY_SEPERATOR','/');
+include 'config.php';
 
 class bookScan
 {
@@ -17,23 +17,54 @@ class bookScan
 			$testPath = $repository.DIRECTORY_SEPERATOR.$subPath;
 			if(!is_dir($testPath) and strpos(strtoupper($subPath),'.MP3') !== false)
 			{
-				echo $testPath." is a book file! ".$repository."\n";
 				$this->bookPathList[] = $repository;
 				$pathParts = explode(DIRECTORY_SEPERATOR,$repository);
 				$this->bookTitles[] = $pathParts[count($pathParts)-1];
 			}
 			else if(is_dir($testPath))
 			{
-				echo "Scan sub sirectory: $testPath\n";
 				$this->scanFiles($testPath);
 			}
 		}
 	}
+	public function syncBooks($repository = "repository")
+	{
+		$this->scanFiles($repository);
+		$this->bookPathList = array_unique($this->bookPathList);
+		$this->bookTitles = array_unique($this->bookTitles);
+		$googleApi = new googleApi();
+		foreach($this->bookTitles as $title)
+		{
+			$response = $googleApi->searchByTitle($title);
+			$pathKey = array_search($title,$this->bookPathList);
+			var_dump($this->bookPathList[$pathKey]);
+			$response->path = $this->bookPathList[$pathKey];
+			return $response;
+		}
+	}
+}
+
+class googleApi
+{
+	public function searchByTitle($title)
+	{
+		$titleSearch = urlencode($title);
+		$request = 'https://www.googleapis.com/books/v1/volumes?q=' . $titleSearch . '&key='. GOOGLE_API_KEY;
+		var_dump($request);
+		$response = file_get_contents($request);
+		$responseObj = json_decode($response);
+		$item = new stdClass();
+		$item->title = $responseObj->items[0]->volumeInfo->title;
+		$item->author = $responseObj->items[0]->volumeInfo->authors[0];
+		$item->publishDate = $responseObj->items[0]->volumeInfo->publishedDate;
+		$item->rating = $responseObj->items[0]->volumeInfo->averageRating;
+		$item->ratingsCount = $responseObj->items[0]->volumeInfo->ratingsCount;
+		$item->thumbnail = $responseObj->items[0]->volumeInfo->imageLinks->thumbnail;
+		$item->description = $responseObj->items[0]->volumeInfo->description;
+		return $item;
+	}
 }
 
 $bookScan = new bookScan();
-$bookScan->scanFiles();
-$bookScan->bookPathList = array_unique($bookScan->bookPathList);
-$bookScan->bookTitles = array_unique($bookScan->bookTitles);
-var_dump($bookScan->bookPathList);
-var_dump($bookScan->bookTitles);
+$response = $bookScan->syncBooks();
+var_dump($response);
